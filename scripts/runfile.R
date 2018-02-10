f514ccb39925d4d9698322eccfdbd51d790066d1
jonathan <jonathanecm@icloud.com>
2018-02-10 14:50:33 -0500

    Changes

--- scripts/runfile.R
+++ scripts/runfile.R
@@ -19,9 +19,9 @@ for (path in paths) {
 }
 
 #source("./scripts/01_sample_files.R") # To activiate this code full corpus of data is needed. 
-source("./scripts/02_extract_p-values.R")
+#source("./scripts/02_extract_p-values.R")
 source("./scripts/03_tidy_data.R")
-# source("./scripts/04_analysis.R")
-#rmarkdown::render_site()
+source("./scripts/04_analysis.R")
+rmarkdown::render_site()
 
 
